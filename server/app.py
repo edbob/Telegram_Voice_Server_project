df26ebf8321d3126de5e232f8b6f5937152f027e
edbob <1984nedov@gmail.com>
2025-06-18 12:23:25 +0300

    Обновление проекта

--- server/app.py
+++ server/app.py
@@ -102,4 +102,5 @@ def upload_file():
     return 'Файл получен', 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
