5a2b53b63ec6f0fda7f32b921b299b0a20366388
edbob <1984nedov@gmail.com>
2025-06-20 21:47:18 +0300

    Обновление проекта

--- server/app.py
+++ server/app.py
@@ -78,5 +78,5 @@ def upload_file():
     return 'Файл получен', 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
--- server/start_all.py
+++ server/start_all.py
@@ -8,19 +8,15 @@ from multiprocessing import Process
 from server.app import app
 
 def run_bot():
-    api_id = int(os.environ['27893983'])
-    api_hash = os.environ['7333605e802b401937e72688aeaa1ea3']
-    phone = os.environ['+380979493781']
-    target_chat = int(os.environ['-1002129469860'])
-    source_channels = list(map(int, os.environ['-1001455546058, -1001594135954, -1001302199689, -1001498303038'].split(',')))
+    api_id = int(os.environ['API_ID'])
+    api_hash = os.environ['API_HASH']
+    phone = os.environ['PHONE']
+    target_chat = int(os.environ['TARGET_CHAT'])
+    source_channels = list(map(int, os.environ['SOURCE_CHANNELS'].split(',')))
+
 
     bot = TelegramVoiceBot(api_id, api_hash, phone, source_channels, target_chat)
     asyncio.run(bot.start())
 
-def run_server():
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
 if __name__ == "__main__":
     Process(target=run_bot).start()
-    run_server()
