8493078416174df5902f6db407c554c555a6b0ca
edbob <1984nedov@gmail.com>
2025-06-20 13:36:31 +0300

    up sratt_all.py

--- server/start_all.py
+++ server/start_all.py
@@ -1,14 +1,23 @@
 # start_all.py
 import subprocess
 import time
+import os
+import sys
 
-# Запускаем telegram-бота в фоне
-print("Запуск telegram_bot.py...")
-bot_proc = subprocess.Popen(["python3", "-m", "bot.telegram_bot"])
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Подождём 3-5 секунд, чтобы бот успел подключиться
-time.sleep(5)
+import asyncio
+from multiprocessing import Process
+from bot.telegram_bot import bot
+from server.app import app
 
-# Запускаем Flask-приложение
-print("Запуск Flask app.py...")
-subprocess.call(["python3", "server/app.py"])
+def run_bot():
+    asyncio.run(bot.start())
+
+def run_server():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    Process(target=run_bot).start()
+    Process(target=run_server).start()
